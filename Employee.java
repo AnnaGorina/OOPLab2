95669487ecb596fdbcaf774b1831d12b665c0578
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-11-24 12:05:36 +0300

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Employee extends Person {
+
+    private int CompanyCount = 0;
+
+    public Employee (String Surname, String Name, String Patronymic) {
+        super(Surname, Name, Patronymic);
+    }
+
+
+
+    public int GetCountCompany () {
+        return CompanyCount;
+    }
+
+    public boolean JoinACompany (Boss boss) {
+        if (boss.JoinAPerson(this)) {
+            CompanyCount++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void LeaveACompany (Boss boss) {
+        boss.DismissPerson(this);
+    }
+
+    @Override
+    public String toString () {
+        return GetSurnamePerson()
+                + " " + GetNamePerson()
+                + " " + GetPatronymicPerson();
+    }
+}
