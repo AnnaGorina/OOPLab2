1eb05f761a2408485241b8f179ee8b5025d36663
AnnaGorina <49533436+AnnaGorina@users.noreply.github.com>
2019-11-25 23:25:07 +0300

    Update and rename main.java to com.company/main.java

--- com.company/main.java
+++ com.company/main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+import CompanyPackage.Boss;
+import CompanyPackage.Company;
+import CompanyPackage.Employee;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Boss BossMicrosoft = new Boss(new Company("Microsoft"), "Иванов", "Иван", "Иванович");
         Boss BossSamsung = new Boss(new Company("Samsung"), "Павлов", "Павел", "Павлович");
+        Boss BossApple = new Boss(new Company("Apple"), "Алексеев", "Алексей", "Алексеевич");
+        Boss BossCisco = new Boss(new Company("Cisco"), "Евгеньев", "Евгений", "Евгеньевич");
+        Boss BossOracle = new Boss(new Company("Oracle"), "Владимиров", "Владимир", "Владимирович");
+        Boss BossIntel = new Boss(new Company("Intel"), "Сергеев", "Сергей", "Сергеевич");
 
         Employee FirstEmployee = new Employee("Петров", "Петр", "Петрович");
         Employee SecondEmployee = new Employee("Александров", "Александр", "Александрович");
@@ -29,5 +37,22 @@ public class Main {
         BossSamsung.DismissPerson(ThirdEmployee);
 
         System.out.println(BossSamsung);
+
+        BossApple.JoinAPerson(FirstEmployee);
+        BossCisco.JoinAPerson(FirstEmployee);
+        BossIntel.JoinAPerson(FirstEmployee);
+        BossCisco.JoinAPerson(FirstEmployee);
+
+        System.out.println("*******************************");
+
+        System.out.println(BossOracle);
+
+        BossApple.DismissPerson(FirstEmployee);
+        BossOracle.JoinAPerson(FirstEmployee);
+
+        System.out.println("*******************************");
+
+        System.out.println(BossOracle);
+        
     }
 }
